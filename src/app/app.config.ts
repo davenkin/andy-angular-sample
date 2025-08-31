c5f9cc7d99bb7f19eb04ae4a47e98979aa25fd7d
Teng Yun <davenkin@126.com>
2025-08-31 15:42:00 +0800

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,22 @@
 import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
-import Material from '@primeuix/themes/material';
+import Lara from '@primeuix/themes/Lara';
 
 import {routes} from './app.routes';
 import {all} from "primelocale";
 import {providePrimeNG} from 'primeng/config';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
+    provideAnimationsAsync(),
     providePrimeNG({
       translation: all.zh_CN,
       theme: {
-        preset: Material
+        preset: Lara
       }
     })
   ]
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
 import {Button} from 'primeng/button';
+import {DatePicker} from 'primeng/datepicker';
+import {FormsModule} from '@angular/forms';
+import {IconField} from 'primeng/iconfield';
+import {InputIcon} from 'primeng/inputicon';
+import {InputText} from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Button],
+  imports: [Button, FormsModule, DatePicker, IconField, InputIcon, InputText],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('andy-angular-sample');
+  date: Array<Date | null> = [];
+
+  ha() {
+    console.info('hello');
+  }
 }
