b38d683d9946bc491bc05f4c5a1e97a63194d31b
Teng Yun <davenkin@126.com>
2025-10-04 08:53:03 +0800

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,9 +6,11 @@ import {routes} from './app.routes';
 import {all} from "primelocale";
 import {providePrimeNG} from 'primeng/config';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
