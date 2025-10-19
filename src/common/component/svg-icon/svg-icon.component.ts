56b4f34c731bc085dac929f29ee9b80abab35028
Teng Yun <davenkin@126.com>
2025-10-19 20:58:24 +0800

    update

---
+++ src/common/component/svg-icon/svg-icon.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-svg-icon',
+  templateUrl: './svg-icon.component.html',
+  styleUrl: './svg-icon.component.scss'
+})
+export class SvgIconComponent {
+  @Input({required: true}) name: string;
+  @Input() alt: string = 'icon';
+}
