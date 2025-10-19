e4aa8e85bf8f304e7399e4488f813d8f8b7b595d
Teng Yun <davenkin@126.com>
2025-10-19 20:49:29 +0800

    update

--- src/common/model/common.model.ts
+++ src/common/model/common.model.ts
@@ -5,9 +5,23 @@ export enum OsType {
   MACOS = 'MACOS'
 }
 
+export const OS_TYPE_NAMES: Map<OsType, string> = new Map([
+  [OsType.WINDOWS, 'Windows'],
+  [OsType.LINUX, 'Linux'],
+  [OsType.ANDROID, 'Android'],
+  [OsType.MACOS, 'MacOS']
+]);
+
 export enum CpuArchitecture {
   X86 = 'X86',
   X86_64 = 'X86_64',
   ARM = 'ARM',
   ARM_64 = 'ARM_64'
 }
+
+export const CPU_ARCHITECTURE_NAMES: Map<CpuArchitecture, string> = new Map([
+  [CpuArchitecture.X86, 'x86'],
+  [CpuArchitecture.X86_64, 'x86_64'],
+  [CpuArchitecture.ARM, 'ARM'],
+  [CpuArchitecture.ARM_64, 'ARM64']
+]);
