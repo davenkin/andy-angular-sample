c31e3501e2fde8ca3a8bc9621b289221d50d0220
Teng Yun <davenkin@126.com>
2025-11-26 21:58:54 +0800

    udpate

--- src/public/design-system/design-system.page.ts
+++ src/public/design-system/design-system.page.ts
@@ -1,11 +1,16 @@
 import {Component} from '@angular/core';
 import iconRegistry from 'common/component/icon/icon-registry';
 import {IconComponent, IconType} from 'common/component/icon/icon.component';
+import {ButtonDirective, ButtonLabel} from 'primeng/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-design-system-page',
   imports: [
-    IconComponent
+    IconComponent,
+    RouterLink,
+    ButtonLabel,
+    ButtonDirective
   ],
   templateUrl: './design-system.page.html',
   styleUrl: './design-system.page.scss'
