0e0bcb631b013edb0bc5f7ab7a4e78f474884755
Teng Yun <davenkin@126.com>
2025-11-26 20:46:02 +0800

    update

--- src/public/design-system/design-system.page.ts
+++ src/public/design-system/design-system.page.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import iconRegistry from 'common/component/icon/icon-registry';
+import {IconComponent, IconType} from 'common/component/icon/icon.component';
 
 @Component({
   selector: 'app-design-system-page',
-  imports: [],
+  imports: [
+    IconComponent
+  ],
   templateUrl: './design-system.page.html',
   styleUrl: './design-system.page.scss'
 })
 export class DesignSystemPage {
 
+  get allIcons() {
+    return Object.keys(iconRegistry) as IconType[];
+  }
 }
