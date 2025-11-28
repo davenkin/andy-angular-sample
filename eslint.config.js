c7a147d841202826fb24dc963da3659573d6bbc6
Teng Yun <davenkin@126.com>
2025-11-29 00:06:02 +0800

    update

--- eslint.config.js
+++ eslint.config.js
@@ -36,7 +36,7 @@ module.exports = tseslint.config(
   },
   {
     files: ['**/*.html'],
-    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility, eslintConfigPrettier],
     rules: {},
   },
 );
