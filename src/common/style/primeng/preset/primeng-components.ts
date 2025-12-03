e96fbe98e25138367923018b532e6e49007085e8
Teng Yun <davenkin@126.com>
2025-12-03 21:39:56 +0800

    update

--- src/common/style/primeng/preset/primeng-components.ts
+++ src/common/style/primeng/preset/primeng-components.ts
@@ -75,4 +75,33 @@ export const PRIMENG_COMPONENTS = {
       },
     },
   },
+  toast: {
+    root: {
+      borderWidth: 'var(--1px)',
+      width: 'var(--600px)',
+    },
+    content: {
+      padding: 'var(--18px)',
+    },
+    colorScheme: {
+      light: {
+        info: {
+          borderColor: 'transparent',
+          color: 'var(--primary-color)',
+        },
+        success: {
+          borderColor: 'transparent',
+          color: 'var(--success-color)',
+        },
+        warn: {
+          borderColor: 'transparent',
+          color: 'var(--warn-color)',
+        },
+        error: {
+          borderColor: 'transparent',
+          color: 'var(--error-color)',
+        },
+      },
+    },
+  },
 };
