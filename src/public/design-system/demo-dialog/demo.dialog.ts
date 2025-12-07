ed32cc8a50272e29e7e8e09bb35fffd8cb4eff76
Teng Yun <davenkin@126.com>
2025-12-07 11:14:52 +0800

    update

--- src/public/design-system/demo-dialog/demo.dialog.ts
+++ src/public/design-system/demo-dialog/demo.dialog.ts
@@ -1,12 +1,23 @@
 import { Component, inject } from '@angular/core';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { Button } from 'primeng/button';
+import { DialogCloseResult } from 'common/service/dialog.service';
 
 @Component({
   selector: 'app-demo-dialog',
   templateUrl: './demo.dialog.html',
   styleUrl: './demo.dialog.scss',
+  imports: [Button],
 })
 export class DemoDialog {
   protected dialogConfig = inject(DynamicDialogConfig);
-  protected dialogRef = inject(DynamicDialogRef);
+  private dialogRef = inject(DynamicDialogRef);
+
+  protected cancel() {
+    this.dialogRef.close(DialogCloseResult.CANCELLED);
+  }
+
+  protected save() {
+    this.dialogRef.close(DialogCloseResult.SUCCEED);
+  }
 }
