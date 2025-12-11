a14c58926eec0c4539369c9f37a372a92604b9c2
Teng Yun <davenkin@126.com>
2025-12-11 20:00:57 +0800

    update

--- src/public/home-page/home.page.ts
+++ src/public/home-page/home.page.ts
@@ -1,25 +1,8 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { Button } from 'primeng/button';
-import { IconComponent } from 'common/component/icon/icon.component';
-import { ToastService } from 'common/service/toast.service';
-import { TranslatePipe } from '@ngx-translate/core';
-import { CurrentContextService } from 'common/service/current-context.service';
-import { DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
-  imports: [RouterLink, Button, IconComponent, TranslatePipe, DatePipe],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
 })
-export class HomePage {
-  private toastService = inject(ToastService);
-  protected currentContext = inject(CurrentContextService);
-
-  toast() {
-    this.toastService.success('成功完成任务');
-  }
-
-  protected readonly Date = Date;
-}
+export class HomePage {}
